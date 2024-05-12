3fbd4f04d270fb433bbaad59b6ae0a480b665d9e
Lyle Liao <lyle.stack.life@gmail.com>
2024-05-12 20:47:59 +0800

    update: thank-you page with phone preview

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
 import { getPaymentStatus } from "./actions";
 import { Loader2 } from "lucide-react";
+import PhonePreview from "@/components/PhonePreview";
 import { formatPrice } from "@/lib/utils";
 
 const ThankYou = () => {
@@ -21,7 +22,7 @@ const ThankYou = () => {
     return (
       <div className="mt-24 flex w-full justify-center">
         <div className="flex flex-col items-center gap-2">
-          <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+          <Loader2 className="h-8 w-8 animate-spin text-stone-500" />
           <h3 className="text-xl font-semibold">Loading your order...</h3>
           <p>This won&apos;t take long.</p>
         </div>
@@ -33,7 +34,7 @@ const ThankYou = () => {
     return (
       <div className="mt-24 flex w-full justify-center">
         <div className="flex flex-col items-center gap-2">
-          <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+          <Loader2 className="h-8 w-8 animate-spin text-stone-500" />
           <h3 className="text-xl font-semibold">Verifying your payment...</h3>
           <p>This might take a moment.</p>
         </div>
@@ -41,7 +42,8 @@ const ThankYou = () => {
     );
   }
 
-  const { billingAddress, shippingAddress, amount } = data;
+  const { configuration, billingAddress, shippingAddress, amount } = data;
+  const { color } = configuration;
 
   return (
     <div className="bg-white">
@@ -51,22 +53,22 @@ const ThankYou = () => {
           <h1 className="mt-2 text-4xl font-bold tracking-tight sm:text-5xl">
             Your case is on the way!
           </h1>
-          <p className="mt-2 text-base text-zinc-500">
+          <p className="mt-2 text-base text-stone-500">
             We&apos;ve received your order and are now processing it.
           </p>
 
           <div className="mt-12 text-sm font-medium">
-            <p className="text-zinc-900">Order number</p>
-            <p className="mt-2 text-zinc-500">{orderId}</p>
+            <p className="text-stone-900">Order number</p>
+            <p className="mt-2 text-stone-500">{orderId}</p>
           </div>
         </div>
 
-        <div className="mt-10 border-t border-zinc-200">
+        <div className="mt-10 border-t border-stone-200">
           <div className="mt-10 flex flex-auto flex-col">
-            <h4 className="font-semibold text-zinc-900">
+            <h4 className="font-semibold text-stone-900">
               You made a great choice!
             </h4>
-            <p className="mt-2 text-sm text-zinc-600">
+            <p className="mt-2 text-sm text-stone-600">
               We at CaseCobra believe that a phone case doesn&apos;t only need
               to look good, but also last you for the years to come. We offer a
               5-year print guarantee: If you case isn&apos;t of the highest
@@ -75,11 +77,18 @@ const ThankYou = () => {
           </div>
         </div>
 
+        <div className="mt-4 flex space-x-6 overflow-hidden rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl">
+          <PhonePreview
+            croppedImageUrl={configuration.croppedImageUrl!}
+            color={color!}
+          />
+        </div>
+
         <div>
           <div className="grid grid-cols-2 gap-x-6 py-10 text-sm">
             <div>
               <p className="font-medium text-gray-900">Shipping address</p>
-              <div className="mt-2 text-zinc-700">
+              <div className="mt-2 text-stone-700">
                 <address className="not-italic">
                   <span className="block">{shippingAddress?.name}</span>
                   <span className="block">{shippingAddress?.street}</span>
@@ -91,7 +100,7 @@ const ThankYou = () => {
             </div>
             <div>
               <p className="font-medium text-gray-900">Billing address</p>
-              <div className="mt-2 text-zinc-700">
+              <div className="mt-2 text-stone-700">
                 <address className="not-italic">
                   <span className="block">{billingAddress?.name}</span>
                   <span className="block">{billingAddress?.street}</span>
@@ -103,33 +112,33 @@ const ThankYou = () => {
             </div>
           </div>
 
-          <div className="grid grid-cols-2 gap-x-6 border-t border-zinc-200 py-10 text-sm">
+          <div className="grid grid-cols-2 gap-x-6 border-t border-stone-200 py-10 text-sm">
             <div>
-              <p className="font-medium text-zinc-900">Payment status</p>
-              <p className="mt-2 text-zinc-700">Paid</p>
+              <p className="font-medium text-stone-900">Payment status</p>
+              <p className="mt-2 text-stone-700">Paid</p>
             </div>
 
             <div>
-              <p className="font-medium text-zinc-900">Shipping Method</p>
-              <p className="mt-2 text-zinc-700">
+              <p className="font-medium text-stone-900">Shipping Method</p>
+              <p className="mt-2 text-stone-700">
                 DHL, takes up to 3 working days
               </p>
             </div>
           </div>
         </div>
 
-        <div className="space-y-6 border-t border-zinc-200 pt-10 text-sm">
+        <div className="space-y-6 border-t border-stone-200 pt-10 text-sm">
           <div className="flex justify-between">
-            <p className="font-medium text-zinc-900">Subtotal</p>
-            <p className="text-zinc-700">{formatPrice(amount)}</p>
+            <p className="font-medium text-stone-900">Subtotal</p>
+            <p className="text-stone-700">{formatPrice(amount)}</p>
           </div>
           <div className="flex justify-between">
-            <p className="font-medium text-zinc-900">Shipping</p>
-            <p className="text-zinc-700">{formatPrice(0)}</p>
+            <p className="font-medium text-stone-900">Shipping</p>
+            <p className="text-stone-700">{formatPrice(0)}</p>
           </div>
           <div className="flex justify-between">
-            <p className="font-medium text-zinc-900">Total</p>
-            <p className="text-zinc-700">{formatPrice(amount)}</p>
+            <p className="font-medium text-stone-900">Total</p>
+            <p className="text-stone-700">{formatPrice(amount)}</p>
           </div>
         </div>
       </div>
